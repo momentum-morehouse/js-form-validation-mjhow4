04f0fe780757802273a2b154a1e65b2c758faaa0
Marlon Howard <marl04coder@outlook.com>
2020-09-22 20:34:40 -0400

    step 1 complete

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 console.log('Add validation!');
+//step 4
+let totalDiv = document.querySelector('#total')
+    //console.log(totalDiv)
+    //totalDiv.textContent = "parking cost"
+    let error = document.createElement('p')
+    error.textContent = "Fill Out Form Please"
+ let container = document.querySelector(".container")
+//console.log(container)
+container.append(error)
+
+    let form = document.querySelector("#parking-form")
+    //console.log(form)
+    form.addEventListener('submit', (event) => {
+        //console.log("hello")
+        event.preventDefault()
+    let nameDiv = document.querySelector('#name-field')
+    let nameBox = document.querySelector("#name")
+     if (nameBox.value.length === 0) {
+         let nameError = document.createElement("p")
+         nameError.textContent = "Fill Out Your Name"
+         nameDiv.append(nameError)
+     }
+    })
