79aea2b63c77505d76b8af52bc8d585aa9152206
Marlon Howard <marl04coder@outlook.com>
2020-09-22 23:20:49 -0400

    attempt 5 and 6

--- script.js
+++ script.js
@@ -37,14 +37,58 @@ form = document.querySelector("#parking-form")
          cvvDiv.append(cvvError)
          //can't figure out why it prints twice on one submit
      }
-     // step 4
+     // step 4 complete
+     // step 5 incomplete
+     //cant get getDay() to work in this function.
      totalDiv = document.querySelector('#total')
-     numberOfDays = document.querySelector('#days')
-     console.log(numberOfDays.value)
+     numberOfDays = document.querySelector('#days') 
+     let startDay = document.querySelector('#start-date')
+     let x = startDay.value 
+     console.log(x)
+     
+     //console.log(numberofDays)
      numDays = numberOfDays.value
      //console.log(totalDiv)
      totalDiv.textContent = "Total Parking Cost: $" + (numDays * 5)
- 
+     
+     //part of step 6
+    let creditDiv = document.querySelector('#credit-card-field')
+    let creditBox = document.querySelector("#credit-card")
+    let cc = creditBox.value
+    console.log(cc)
+    if (creditBox.value.length !== 16)  {
+    let creditError = document.createElement("p")
+         creditError.textContent = "Credit Card must be 16 digits"
+         creditDiv.append(creditError)
+     }
+
+    //these formulas did not work for me
+    // function validateCardNumber(number) {
+    //     var regex = new RegExp("^[0-9]{16}$");
+    //     if (!regex.test(number))
+    //         return false;
+    
+    //     return luhnCheck(number);
+    // }
+    
+    // function luhnCheck(val) {
+    //     var sum = 0;
+    //     for (var i = 0; i < val.length; i++) {
+    //         var intVal = parseInt(val.substr(i, 1));
+    //         if (i % 2 == 0) {
+    //             intVal *= 2;
+    //             if (intVal > 9) {
+    //                 intVal = 1 + (intVal % 10);
+    //             }
+    //         }
+    //         sum += intVal;
+    //     }
+    //     return (sum % 10) == 0;
+    
+    // } 
+    // validateCardNumber(cc);
+    // luhnCheck(cc);
     })
 
+   
    
